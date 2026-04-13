3f8e278182227a6c49ee090dc75497cf3d18cf6f
irinque <klenovmatvey@yandex.ru>
2026-04-13 19:50:32 +0300

    refactor: reorganize project structure and add CSS variables

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header className='App-header'></header>
+      <main className='App-main'></main>
+    </>
+  );
+}
+
+export default App;
